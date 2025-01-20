c970a23352ea66857d049c259f95ad39379b2536
Dan Gilleland <dgilleland@nait.ca>
2025-01-20 12:35:12 -0700

    Code we typed in node repl

---
+++ src/005/Jan20.js
@@ -0,0 +1,36 @@
+const pi = 3.14159
+let firstNumber
+let secondNumber
+pi
+firstNumber
+firstNumber = 5
+secondNumber = 8
+let name = "Dan Gilleland"
+console.clear()
+firstNumber + secondNumber
+firstNumber - secondNumber
+firstNumber * secondNumber
+firstNumber / secondNumber
+secondNumber / firstNumber
+secondNumber % firstNumber
+console.clear()
+typeof firstNumber
+typeof name
+firstNumber = 'five'
+typeof firstNumber
+firstNumber + secondNumber
+console.clear()
+secondNumber
+secondNumber.toString()
+firstNumber = 5
+firstNumber = '5'  // I can use single or double-quotes for strings
+firstNumber + secondNumber
+parseInt(firstNumber)
+parseFloat(firstNumber)
+let piText = pi.toString()
+piText
+parseInt(piText)
+parseFloat(piText)
+console.clear
+console.clear()
+secondNumber + 7
