b1eff5097aeaf26541b2a0de1ca6f45c20038c52
Dan Gilleland <dgilleland@nait.ca>
2025-01-22 10:45:23 -0700

    Improved function greetUser()

--- src/007/demo-greet/js/main.js
+++ src/007/demo-greet/js/main.js
@@ -1,14 +1,22 @@
 // This demonstrates using a parameter
 // e.g. greetUser('Jane Doe');
+//                \________/    'Jane Doe' is an argument supplied to the function
 // e.g. greetUser(prompt('Name:'));
+
+const originalHeading = document.querySelector('h1.intro').innerText;
 
 // All that this does is declare the function:
 // - identifying the name of the function
 // - identifying the information supplied to the function (parameters)
 // - identify the instructions {body} that will be executed when the function is called
-function greetUser(username) {
+
+// Using a const is a preferred way to declare a function
+const greetUser = function (username) {
+//    \_______/            \________/
+//     | reference/name     | my list of parameter variables
+//                            (comma, separated, list)
     // select the h1
     var mainTitle = document.querySelector('h1.intro');
     // update the h1 inner HTML
-    mainTitle.innerHTML = 'Welcome ' + username + ' to ' + mainTitle.innerHTML;
+    mainTitle.innerHTML = 'Welcome ' + username + ' to ' + originalHeading;
 };
