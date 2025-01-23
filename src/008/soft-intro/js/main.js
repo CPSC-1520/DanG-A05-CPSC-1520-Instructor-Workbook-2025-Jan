e2021c6a46f95db8f7251a541190338753a19185
Dan Gilleland <dgilleland@nait.ca>
2025-01-23 15:11:52 -0700

    First demo of events in JavaScript

---
+++ src/008/soft-intro/js/main.js
@@ -0,0 +1,32 @@
+console.log('JavaScript file loaded!');
+
+// This is a function whose purpose (for this app) is to respond to (or "handle") an event.
+// Specifically, I'm going to use for my 'click' event (see below)
+const handleClick = function(event) {
+    //                       \___/  <--- this is just a parameter name
+    event.preventDefault(); // calling .preventDefault() to tell it to stop doing its default action
+    // TODO: Use for exploration purposes
+    const target = event.target;  // the .target property is the DOM element that received the event
+    document.getElementById('feedback').innerText = `Clicked from ${target.tagName}`;
+    //                                                                    \______/
+    //                         this is the name of the HTML tag (DOM Element)  |
+}
+
+let heading = document.querySelector('h1');
+// This is where I "register" the DOM element to listen for a 'click' event,
+// and then have the browser call my handleClick method when the 'click' event
+// happens inside my heading.
+heading.addEventListener('click', handleClick);
+//                                \_________/ <---- note I am passing in the name of my function
+
+// TODO: Add an event listener for the button with id "register".
+//       Listen for the 'dblclick' event.
+//       Prompt the user for their name, then for their email,
+//       then output the information to the #feedback element.
+
+// TODO: Add another event listener for the heading, this time for
+//       the 'dblclick' event.
+//       Try changing the theme by assigning either 'light' or 'dark'
+//       to the following element's property:
+//          document.querySelector('html').dataset.theme
+//       Try it out, then inspect the elements in the Dev Tools.
