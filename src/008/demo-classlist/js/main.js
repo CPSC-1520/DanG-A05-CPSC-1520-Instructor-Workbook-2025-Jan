73d7ee8bc16ef36f1d14a7a2d971fdf7a5ba84e6
Dan Gilleland <dgilleland@nait.ca>
2025-01-27 11:36:12 -0700

    Demo using .classList DOM property

--- src/008/demo-classlist/js/main.js
+++ src/008/demo-classlist/js/main.js
@@ -2,3 +2,14 @@ console.log('main.js has been loaded');
 const sandbox = document.querySelector('output#sandbox');
 
 // TODO: Use the space below for playing with JavaScript.
+// A quick sample to toggle the display of an item by adding/removing a CSS class
+document.querySelector('h1')
+        .addEventListener('click', function() {
+            let outputTag = document.querySelector('#sandbox');
+            outputTag.classList.toggle('hidden');   // 'hidden' is a class from TailwindCSS
+            // The .classList property of a DOM element has functions for adding/removing
+            // CSS classes from that element.
+            //  - .toggle()  <-- used to add or remove
+            //  - .add()     <-- used to add
+            //  - .remove()  <-- used to remove
+        });
