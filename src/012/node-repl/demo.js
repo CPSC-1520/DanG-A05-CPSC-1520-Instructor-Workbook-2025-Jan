01a4aad5fb674553725dc590429a4096735ab229
Dan Gilleland <dgilleland@nait.ca>
2025-02-05 11:59:27 -0700

    Demo relational and conditional expressions in Node

---
+++ src/012/node-repl/demo.js
@@ -0,0 +1,36 @@
+let textZero = '0';
+let numberZero = 0;
+let numberEight = 8;
+let numberFive = 5;
+let textEight = '8';
+let name = 'Stewart';
+let success = true;
+let failure = false;
+
+// numbers as strings
+let oneThousand = '1000';
+let seven = '7';
+
+// A constant
+const alwaysText = 'Text';
+
+// Sample values from different <input> types
+let inputNumber = '4';  // <input type="number" />
+// Dates/Times are in ISO format
+let inputDate = '2025-02-28';   // <input type="date" />
+let inputTime = '17:43';        // <input type="time" />
+let inputDateTime_Local = "2025-02-28T17:43";   // <input type="datetime-local" />
+
+console.clear()
+(textZero == numberZero)
+textZero
+numberZero
+(textZero === numberZero)
+numberEight
+(numberEight > numberZero)
+seven
+oneThousand
+(oneThousand > seven)
+('Daniel' > 'Dan')
+('Dana' > 'Daniel')
+if(seven) { console.log('Expression is true'); } else { console.log('It is false'); }
